51722f7b5b2f16e01df6ab0579cf4b00bf8fca3c
devsamir <56377354+devsamir@users.noreply.github.com>
2022-10-30 12:02:29 +0700

    make deleted_at field optional in schema

---
+++ prisma/migrations/20221030050202_deleted_at/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE `items` MODIFY `delete_at` DATETIME(3) NULL;
+
+-- AlterTable
+ALTER TABLE `mitra` MODIFY `delete_at` DATETIME(3) NULL;
+
+-- AlterTable
+ALTER TABLE `unit` MODIFY `delete_at` DATETIME(3) NULL;
