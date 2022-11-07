999e295af9542034e9f31d790cd51b2624eea9b0
devsamir <56377354+devsamir@users.noreply.github.com>
2022-11-07 20:59:36 +0700

    add incident details crud functionality in incident module

--- src/Modules/Incident/incident.dto.ts
+++ src/Modules/Incident/incident.dto.ts
@@ -1,5 +1,16 @@
 import { Type } from 'class-transformer';
-import { IsNotEmpty, IsOptional, MaxLength } from 'class-validator';
+import {
+  ArrayMinSize,
+  IsArray,
+  IsBoolean,
+  IsEnum,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+  MaxLength,
+  ValidateNested,
+} from 'class-validator';
 
 import { ExcludeUserDto } from '../User/user.dto';
 
@@ -16,6 +27,14 @@ export class CreateIncidentDto {
   })
   @IsNotEmpty({ message: 'Jenis pekerjaan tidak boleh kosong' })
   job_type: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @ArrayMinSize(1, {
+    message: 'Tiket setidaknya harus memiliki satu material',
+  })
+  @Type(() => CreateIncidentDetails)
+  incident_details: CreateIncidentDetails[];
 }
 
 export class UpdateIncidentDto {
@@ -34,6 +53,71 @@ export class UpdateIncidentDto {
   @IsNotEmpty({ message: 'Jenis pekerjaan tidak boleh kosong' })
   @IsOptional()
   job_type: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @ArrayMinSize(1, {
+    message: 'Tiket setidaknya harus memiliki satu material',
+  })
+  @Type(() => UpdateIncidentDetails)
+  @IsOptional()
+  incident_details: UpdateIncidentDetails[];
+}
+
+enum ApproveWh {
+  not_yet = 'not_yet',
+  approved = 'approved',
+  decline = 'decline',
+}
+
+enum OrmCode {
+  create = 'create',
+  update = 'update',
+  delete = 'delete',
+}
+export class CreateIncidentDetails {
+  @IsNumber({}, { message: 'material harus number' })
+  @IsNotEmpty({ message: 'material tidak boleh kosong' })
+  item_id: number;
+
+  @IsString({ message: 'Uraian pekerjaan harus berupa string' })
+  @IsOptional()
+  job_detail: string;
+
+  @IsNumber({}, { message: 'Jumlah material harus number' })
+  @IsNotEmpty({ message: 'Jumlah material tidak boleh kosong' })
+  qty: number;
+
+  @IsEnum(ApproveWh, { message: 'Approve WH tidak sesuai' })
+  @IsOptional()
+  approve_wh: ApproveWh;
+}
+
+export class UpdateIncidentDetails {
+  @IsNumber({}, { message: 'id harus number' })
+  @IsOptional()
+  id: number;
+
+  @IsNumber({}, { message: 'material harus number' })
+  @IsNotEmpty({ message: 'material tidak boleh kosong' })
+  @IsOptional()
+  item_id: number;
+
+  @IsString({ message: 'Uraian pekerjaan harus berupa string' })
+  @IsOptional()
+  job_detail: string;
+
+  @IsNumber({}, { message: 'Jumlah material harus number' })
+  @IsNotEmpty({ message: 'Jumlah material tidak boleh kosong' })
+  @IsOptional()
+  qty: number;
+
+  @IsEnum(ApproveWh, { message: 'Approve WH tidak sesuai' })
+  @IsOptional()
+  approve_wh: ApproveWh;
+
+  @IsEnum(OrmCode, { message: 'ORM Code tidak boleh kosong' })
+  orm_code: OrmCode;
 }
 
 export class ExcludeIncidentDto {
