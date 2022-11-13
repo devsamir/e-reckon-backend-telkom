7364fccee083e20b30de99f74ee7f30804eb065c
devsamir <56377354+devsamir@users.noreply.github.com>
2022-11-13 13:53:29 +0700

    make sort query more flexible

--- src/Common/helpers/generateQuery.ts
+++ src/Common/helpers/generateQuery.ts
@@ -8,15 +8,24 @@ export const generateQuery = (query: GetAllQuery) => {
     (acc, curr) => ({ ...acc, [curr]: true }),
     {},
   );
+  const sort = query.sort?.split(' ');
+  const sortValue = sort?.[1] || 'desc';
+  const sortFields = sort?.[0]?.split('.') || ['id'];
+  const sortQuery = {};
+  sortFields.reduce((acc, curr, idx) => {
+    if (idx < sortFields.length - 1) {
+      acc[curr] = {};
+    } else {
+      acc[curr] = sortValue;
+    }
+    return acc[curr];
+  }, sortQuery);
 
-  const sort = query?.sort?.split(' ');
   return {
     where: query.domain,
     skip: query.offset,
     take: query.limit,
-    orderBy: {
-      [sort?.[0] || 'id']: sort?.[1] || 'desc',
-    },
+    orderBy: sortQuery,
     select: selectedFields,
     ...(query?.include?.length ? { include: includedModel } : {}),
   };
